83be0a0edcd1f5c11dddf79c97ea75e89222f0bc
osfunapps <osfunapps@email.com>
2019-06-27 08:46:30 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,102 @@
+Introduction
+------------
+
+This project meant to provide intuitive functions to use Google's Puppeteer, without wasting time.
+
+
+## Installation and use
+
+Install via npm:
+    
+    npm i os-puppeteer-helper
+        
+Require ph:
+        
+    var ph = require("os-puppeteer-helper")
+                
+Create a browser:     
+    
+    let bt = await ph.createBrowser(url, slowMo = 5, headless = false, width = 1300, height = 768)
+        
+    // save the page and the browser
+    let browser = bt[0];
+    let page = bt[1];
+        
+Navigate to a page:     
+    
+    await ph.navigateTo(page, url, givenTimeout = null, waitForSelector = null, timeoutSelectorSearch = 24000,
+                                                                                delayAfterSelector = 1500,
+                                                                                delayAfterNavigated = 0) {
+Insert text by selectors:
+
+    await ph.setText(page, selector, text, delayAfter = 0, typeDelay = 20, clearTextBefore = true)
+                           
+Click and wait for selector:
+        
+    await ph.click(page, selector, delayAfterClick = 0, selectorToFindAfterClick = null, howLongToWaitForSelector = 240000, delayAfterSelectorFound = 1500) 
+      
+Wait for selector to be removed from the dom:
+    
+    await ph.waitForSelectorToBeRemoved(page, selector, checkEach = 2000)
+        
+Click on element contains certain text:
+
+    await ph.clickOnElementContainsText(page, selector, text, delayAfterClick = 0, selectorToFindAfterClick = null, howLongToWaitForSelector = null, delayAfterSelectorFound = 1500) { 
+
+Download a file:
+    
+    await ph.downloadFile(page, path, downloadSelector)     
+
+And a lot more!
+
+## Example
+
+a short example of initiating a purchase from ebay:   
+
+    // create the browser
+    let tuplee = await ph.createBrowser("about:blank");
+    let browser = tuplee[0];
+    let page = tuplee[1];
+            
+    // navigate to eBay
+    await ph.navigateTo(page, 'https://www.ebay.com/signin/');
+
+    // insert username and pass
+    await ph.setText(page, "#userid", ebayUsername);
+    await ph.setText(page, "#pass", ebayPas);
+    
+    // click ok and wait
+    await ph.click(page, "#sgnBt");
+    console.log("waiting for selector...
+    await ph.waitForSelector(page, "form[id = 'gh-f'");
+    console.log("done waiting!");
+
+
+    // navigate to a shopping page and wait
+    await ph.navigateTo(page, EBAY_LINK, null, "label[for = 'qtyTextBox']", null);
+    
+    // clear the quantity box and set it with your receipt obj quantity 
+    await ph.clearText(page, "input[id = 'qtyTextBox']");
+    await ph.setText(page, "input[id = 'qtyTextBox']", receipt.quantity);
+
+    // click on buy
+    await ph.click(page, "#binBtn_btn");    
+    
+    // at the make a purchase page, wait for the quantity, at the bottom, to appear
+    await ph.waitForSelector(page, "div.item-image");            
+
+    //...
+    
+
+## Additional notes
+
+If you're trying to web scrape, consider integrate this module with [os-cheerio-helper](https://www.npmjs.com/package/os-cheerio-helper) for more professional use.  
+
+## Links
+[npm os-puppeteer-helper](https://www.npmjs.com/package/os-puppeteer-helper)
+
+## Licence
+ISC
+
+
+# os-puppeteer-helper-npm
---
+++ index.js
@@ -0,0 +1,234 @@
+const puppeteer = require('puppeteer');
+const tools = require('os-tools');
+
+const self = module.exports = {
+
+    /**
+     * will create a new chromium browser
+     * @param url -> the url to log in to
+     * @param slowMo -> how long to wait before each command
+     * @param headless -> show/hide browser
+     * @param width -> browser width
+     * @param height -> browser height
+     */
+    createBrowser: async function (url, slowMo = 5, headless = false, width = 1300, height = 768) {
+        const browser = await puppeteer.launch({
+            headless: headless,
+            slowMo: slowMo // slow down by 5
+        });
+
+        const page = await browser.newPage();
+        await page.setViewport({width: width, height: height});
+        await self.navigateTo(page, url);
+        return [browser, page]
+    },
+
+    /**
+     * will navigate to a certain url with an option to search for a selector after page load
+     */
+    navigateTo: async function (page,
+                                url,
+                                givenTimeout = null,
+                                waitForSelector = null,
+                                timeoutSelectorSearch = null,
+                                delayAfterSelector = 1500,
+                                delayAfterNavigated = 0) {
+        await page.goto(url, {waitUntil: 'load', timeout: givenTimeout});
+        await tools.delay(delayAfterNavigated);
+        if (waitForSelector != null) {
+            await self.waitForSelector(page, waitForSelector, timeoutSelectorSearch, delayAfterSelector)
+        }
+    },
+
+    /**
+     * will wait for an element to appear.
+     * @param page -> the puppeteer page
+     * @param selector -> the selector to search for
+     * @param timeout -> 0 to disable timeout
+     * @param delayAfterFound -> how long to wait after found
+     */
+    waitForSelector: async function (page, selector, timeout = null, delayAfterFound = 1500) {
+        await page.waitForSelector(selector, {timeout: timeout});
+        await tools.delay(delayAfterFound)
+    },
+
+    /**
+     * will wait for an element to be removed from the dom
+     */
+    waitForSelectorToBeRemoved: async function (page, selector, checkEach = 2000) {
+        while (true) {
+            try {
+                await tools.delay(checkEach);
+                await self.waitForSelector(page, selector, null, 0)
+            } catch (error) {
+                return
+            }
+        }
+    },
+
+    /**
+     * will wait for an elements to appear
+     */
+    waitForSelectors: async function (page, timeout = null, delayAfterFound = 1500, ...selectors) {
+        await page.waitForSelector(selectors, {timeout: timeout});
+        await tools.delay(delayAfterFound)
+    },
+
+
+    /**
+     * will wait for navigation to change
+     */
+    waitForNavigation: async function (page, selector, timeout = null) {
+        return await page.waitForNavigation(selector, {timeout: timeout});
+    },
+
+
+    /**
+     * will add text to an element
+     */
+    setText: async function (page, selector, text, delayAfter = 0, typeDelay = 20, clearTextBefore = true) {
+        if (clearTextBefore) {
+            await self.clearText(page, selector, 500)
+        }
+        await page.type(selector, text, {delay: typeDelay});
+        await tools.delay(delayAfter)
+    },
+
+    /**
+     * will click a selector
+     * @param page -> the current page
+     * @param selector -> the selector to click. For example: a:nth-of-type(2)
+     * @param delayAfterClick -> optional delay after click
+     * @param selectorToFindAfterClick -> optional element to look for after click
+     * @param howLongToWaitForSelector -> optional time to look for the element after click
+     * @param delayAfterSelectorFound ->  optional delay after element found
+     */
+    click: async function (page,
+                           selector,
+                           delayAfterClick = 0,
+                           selectorToFindAfterClick = null,
+                           howLongToWaitForSelector = null,
+                           delayAfterSelectorFound = 1500) {
+
+        await page.click(selector);
+        await tools.delay(delayAfterClick);
+        if (selectorToFindAfterClick != null) {
+            await self.waitForSelector(page, selectorToFindAfterClick, howLongToWaitForSelector, delayAfterSelectorFound)
+        }
+    },
+
+    /**
+     * Will selectByValue an element based on it's value
+     * For more formidable approach, look for CheerioHelper.isSelectHasValue()
+     */
+    selectByValue: async function (page, selector, value, delayAfter = 0) {
+        await page.select(selector, value);
+        await tools.delay(delayAfter)
+    },
+
+
+    /**
+     * will clear text from an element
+     */
+    clearText: async function (page, selector, delayAfter = 0) {
+        await page.evaluate(function (selector) {
+            // this code has now has access to foo
+            document.querySelector(selector).value = "";
+        }, selector);
+        await tools.delay(delayAfter)
+    },
+
+    /**
+     * will create a count down (10 9 8 ...)
+     */
+    makeCountDown: async function (from) {
+        for (let i = from; i > 0; i--) {
+            console.log(i);
+            await tools.delay(1000)
+        }
+    },
+
+    /**
+     * will download a file
+     * @param page -> the puppeteer page
+     * @param path -> the path to the file
+     * @param downloadSelector -> the selector to click in order to start the download
+     */
+    downloadFile: async function (page, path, downloadSelector) {
+        await page._client.send('Page.setDownloadBehavior', {
+            behavior: 'allow',
+            downloadPath: path
+        });
+        await self.click(page, downloadSelector)
+    },
+
+    /**
+     * will click on an element which contains text
+     * for example("div", "Floki") will click on a div with an innerHTML of "Floki" (uppercase sensitive)
+     * @param page -> the puppeteer page
+     * @param selector -> the selector to click. For example: a:nth-of-type(2)
+     * @param text -> the element's innerHTML
+     * @param delayAfterClick -> optional delay after click
+     * @param selectorToFindAfterClick -> optional element to look for after click
+     * @param howLongToWaitForSelector -> optional time to look for the element after click
+     * @param delayAfterSelectorFound ->  optional delay after element found
+     */
+    clickOnElementContainsText: async function (page,
+                                                selector,
+                                                text,
+                                                delayAfterClick = 0,
+                                                selectorToFindAfterClick = null,
+                                                howLongToWaitForSelector = null,
+                                                delayAfterSelectorFound = 1500) {
+        await page.evaluate(function (selector, text) {
+            // this code has now has access to foo
+            let allEle = document.querySelectorAll(selector);
+            for (let i = 0; i < allEle.length; i++) {
+                if (allEle[i].innerHTML === text) {
+                    allEle[i].click()
+                }
+            }
+
+        }, selector, text);
+
+        await tools.delay(delayAfterClick);
+        if (selectorToFindAfterClick != null) {
+            await self.waitForSelector(page, selectorToFindAfterClick, howLongToWaitForSelector, delayAfterSelectorFound)
+        }
+    },
+
+
+    /**
+     * will return element/s from the dom
+     */
+    getElements: async function (page, selector) {
+        return await page.$$(selector);
+    },
+
+    /**
+     * will return element/s from the dom
+     */
+    getElementsFromElement: async function (element, attName) {
+        let children =  await (await element.getProperty('selector')).jsonValue();
+        let firstChild = children[0]
+        let dVal = await self.getAttributeValueFromElement(firstChild, 'd')
+
+    },
+
+    /**
+     * will return an attribute value from an element
+     */
+    getAttributeValueFromElement: async function (element, attName) {
+        return await (await element.getProperty(attName)).jsonValue();
+    },
+
+    /**
+     * will return the inner html of an element
+     */
+    getInnerHTMLFromElement: async function (element) {
+        return await (await element.getProperty('innerHTML')).jsonValue();
+    },
+
+
+};
+
